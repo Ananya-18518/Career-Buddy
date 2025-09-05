2c4d3b463b0e776754ecfe7916d328ea9b659d9d
Ananya-18518 <a23549631@gmail.com>
2025-09-05 17:08:03 +0530

    fix routing issue

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+const redirect = new URLSearchParams(window.location.search).get("redirect");
+if (redirect) {
+  window.history.replaceState(null, "", redirect);
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
